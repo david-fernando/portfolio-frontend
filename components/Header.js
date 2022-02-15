b30d09df12be6232853f70e926b76903347a443a
David Fernando <davidfernandodamata21@gmail.com>
2022-02-15 21:43:46 +0000

    Fix responsive photo

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ function Header(){
     <header className={styles.headerContainer}>
       <span>
         <div className={styles.imageContainer}>
-          <Image src={photo} layout="fill" className={styles.image} alt="David Fernando" />
+          <Image src={photo} layout="responsive" className={styles.image} alt="David Fernando" />
         </div>
         <div className={styles.profile}>
           <span>David Fernando</span>
