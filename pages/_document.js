0c5a0a8d40fe5679249433cf9c89bad09a788f78
David Fernando <davidfernandodamata21@gmail.com>
2022-08-21 21:59:39 +0000

    Add tag link

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-      <Head />
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet" />
+      </Head>
         <body>
           <Main />
           <NextScript />
