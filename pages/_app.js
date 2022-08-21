554898526ae10790d9bd31206d68e8d659e03054
David Fernando <davidfernandodamata21@gmail.com>
2022-08-21 21:59:28 +0000

    Remove tag link

--- pages/_app.js
+++ pages/_app.js
@@ -9,8 +9,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>David Fernando</title>
         <meta name="description" content="Portfolio desenvolvedor front end" />
-        <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet" /> 
+        <link rel="icon" href="/favicon.ico" /> 
       </Head>       
       <Component {...pageProps} />
     </>
